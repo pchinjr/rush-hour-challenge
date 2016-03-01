62b5ef03cad29dd2aba10f864df3c967d9ace5e0
Paul Chin Jr <pchinjr@gmail.com>
2016-03-01 05:06:14 +0000

    another shot

--- rushhour/js/board.js
+++ rushhour/js/board.js
@@ -1,14 +1,29 @@
-function Board(input, exit) {
-	//board state
-	this.spaces = input.board;
-	this.exit = exit;
-	
-	//spaces should know if it's empty and which vehicle is occupying the space
-	
-}
-
-Board.prototype.setVehicle = function() {
-	//puts vehicles on board and moves them around
+var myBoard = {
+	"board": [
+		["1", "1", "1", "*", "*", "*"],
+		["2", "*", "3", "4", "4", "4"],
+		["2", "*", "3", "R", "R", "5"],
+		["6", "7", "7", "8", "*", "5"],
+		["6", "*", "*", "8", "9", "A"],
+		["B", "B", "B", "8", "9", "A"]
+    ]
 };
 
+var Board = function(input) {
+	this.spaces = input.board;
+	this.exit = '[2][5]';
+	this.vehicles = []; 
+	
+};
+
+Board.prototype.placeVehicle = function(size, position, direction) {
+	this.vehicles.push(new Vehicle(size, position, direction));
+}
+
+
+var Vehicle = function(size, position, direction) {
+	this.size = size;
+    this.position = position;
+    this.direction = direction;
+};
 
--- rushhour/js/vehicle.js
+++ rushhour/js/vehicle.js
@@ -1,21 +1,8 @@
-//vehicle objects
+//vehicle object
 
-function Vehicle(name, start, size, direction) {
-    this.name = name;
-    this.size = size;
-    this.start = start;
-    this.direction = direction
-}
-
- 	var v1 = new Vehicle('1', 1, 3, 'x');
-	var v2 = new Vehicle('2', 7, 2, 'y');
-	var v3 = new Vehicle('3', 9, 2, 'y');
-	var v4 = new Vehicle('4', 16, 3, 'x');
-	var v5 = new Vehicle('5', 18, 2, 'y');
-	var v6 = new Vehicle('6', 19, 2, 'y');
-	var v7 = new Vehicle('7', 20, 2, 'x');
-	var v8 = new Vehicle('8', 22, 3, 'y');
-	var v9 = new Vehicle('9', 29, 2, 'y');
-	var va = new Vehicle('A', 30, 2, 'y');
-	var vb = new Vehicle('B', 31, 3, 'x');
-	var vr = new Vehicle('R', 16, 2, 'x');
+// function Vehicle(start, size, direction) {
+//     this.size = size;
+//     this.start = start;
+//     this.direction = direction;
+// }
+ 
