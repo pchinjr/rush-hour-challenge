63c3de276f56e8f419f53367a08442d2fcc14c3f
Paul Chin Jr <pchinjr@gmail.com>
2016-03-02 03:49:17 +0000

    most useless commit evAR

--- rushhour/js/game.js
+++ rushhour/js/game.js
@@ -20,15 +20,47 @@ var Board = function(input) {
 };
 
 //vehicle object 
-var Vehicle = function(size, position, direction) {
+var Vehicle = function(size, direction) {
 	this.size = size;
     this.direction = direction;
+};
+
+Board.prototype.setVehicle = function(size, direction) {
+	board.vehicles.push(new Vehicle(size, direction));
+};
+
+Board.prototype.getVehicle = function(id) {
+	var results = [];
+	for(var x=0; x < 5; x++) {
+		results.push(board.board[x].indexOf(id));
+	}
+	return results;
+};
+
+Board.prototype.moveVehicle = function() {
+	
+}
+
+
+var board = new Board(myBoard);
+board.setVehicle(3, 'x');
+board.setVehicle(2, 'y');
+board.setVehicle(2, 'y');
+
+var render = function() {
+        $('#row1').html(JSON.stringify(board.board[0]));
+        $('#row2').html(JSON.stringify(board.board[1]));
+        $('#row3').html(JSON.stringify(board.board[2]));
+        $('#row4').html(JSON.stringify(board.board[3]));
+        $('#row5').html(JSON.stringify(board.board[4]));
+        $('#row6').html(JSON.stringify(board.board[5]));
+        $('#vehicles').html(JSON.stringify(board.vehicles));
 };
 
 
 
 
-var board = new Board(myBoard);
+
 
 
 
